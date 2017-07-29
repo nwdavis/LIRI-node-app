575cf6caa6df7606c53d8335bacee4055d36df54
nwdavis <nathanieldavis89@gmail.com>
2017-07-28 21:49:13 -0400

    files

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: fKM34DyPHFedY24MUSHJ6bgEP,
+  consumer_secret: bIeKfX5lWhgKjFTtIXkomUg5EQuoedzLRDqVjpmiCX3s2Z2btX,
+  access_token_key: 891103805378633728-AsK47LsoX69p4YNVq1y5llCzg3rxggH,
+  access_token_secret: 	jYVmOZqD50c42HQkaIgQ7ioT238EB22Br5nk5jY0th6HF,
+}
+
+exports.spotifyKeys = {
+ client_id: 8a597cac843c4678a7c169b7eb9b9626,
+ client_secret: 3fbe195219b944bb88c5ebf971ff81d1,
+}
